ef4160ddd530b612d292c2a62dcc9a52d14db8c5
saad <saadmeddiche2004201@gmail.com>
2023-06-01 10:22:43 +0100

    First Commit

---
+++ assets/js/services.js
@@ -0,0 +1,32 @@
+
+var Cards_Content = document.getElementById('Cards_Content_S1');
+
+var Card = `<div class="Card">
+                <div class="Card_Content">
+                <div class="Image_Card">
+                    <img src="./assets/img/1.webp" alt="" />
+                </div>
+                <div class="Title_Card">
+                    <p>
+                        Nettoyage de Bureaux
+                        <br />
+                        <br />
+                        <span>Nettoyage bureaux à Casablanca</span>
+                    </p>
+                </div>
+                <div class="Text_Card">
+                    Le sécialiste du <strong>nettoyage de bureau</strong> à <strong>casablanca</strong> depuis plusieurs d'années garde vos bureaux impeccables.Faites le choix du savoir-faire, de l'expérience et de la qualité; pour le nettoyage de vos bureaux à
+                    <strong>Casablanca</strong> .
+                    Le sécialiste du <strong>nettoyage de bureau</strong> à <strong>casablanca</strong> depuis plusieurs d'années garde vos bureaux impeccables.Faites le choix du savoir-faire, de l'expérience et de la qualité; pour le nettoyage de vos bureaux à
+                    <strong>Casablanca</strong> .
+                    
+                </div>
+                <div class="PLus_Infomration">
+                    <a href="">Savoir Plus ...</a>
+                </div>
+                </div>
+            </div>`;
+
+for(var i = 1 ; i <= 8; i++){
+    Cards_Content.innerHTML+= Card;
+}
---
+++ assets/js/slide.js
@@ -0,0 +1,82 @@
+
+// =======================Declaration of varibales=======================
+var current_index = 1;
+
+var slide = document.getElementById("ContentS1");
+var title = document.getElementById("TitleS1");
+var content_of_title = document.getElementById("TitleS1_content");
+var button = document.getElementById("ButtoS1");
+
+var images = {
+    1:{Link :'./assets/img/ant.webp' , title :'Lutte contre les insectes à Casablanca'},
+    2:{Link :'./assets/img/rats.webp' , title :'Les professionnels de la Dératisation à Casablanca'},
+    3:{Link :'./assets/img/Human.webp' , title :'Les specialites de la desinfection au Maroc'}
+}
+
+
+// =======================Start Up Functions=======================
+change_image_automaticly()
+
+// =======================Main Funtcion=======================
+function Slide(increment){
+    change_image(increment)
+
+    change_title()
+    animate_title()
+
+    animate_button()
+}
+
+// =======================Functions=======================
+function change_title(){
+    content_of_title.innerHTML = images[current_index].title
+}
+
+function change_image(increment){
+    current_index += increment;
+
+    if(current_index > 3 ){
+        current_index = 1
+    }
+
+    if(current_index <= 0 ){
+        current_index = 3
+    }
+   
+    slide.style.backgroundImage= "url('" +images[current_index].Link + "')";
+}
+
+function animate_title(){
+    title.animate(
+        {
+            transform: ["translateY(-50%)", "translateY(0%)"],
+
+        },
+        {
+            duration: 400,
+        }
+    );
+}
+
+function animate_button(){
+    button.animate(
+        {
+            transform: ["translateX(-200%)", "translateX(0%)"],
+
+        },
+        {
+            duration: 800,
+        }
+    );
+}
+
+function change_image_automaticly(){
+
+    setInterval(function(){
+        Slide(1)
+    },4000)
+
+}
+
+  
+
